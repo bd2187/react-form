4b9a7ad82e1fe7829e381aac95f7402e0c6b5e9f
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-04-30 20:13:07 -0700

    Create NameForm component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+class NameForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange() {
+    var value = document.getElementById("name").value;
+    this.setState({ value: value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log("submit");
+  }
+
+  render() {
+    return (
+      <form method="POST" action="/" onSubmit={this.handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <br />
+        <input
+          type="text"
+          placeholder="Input name"
+          id="name"
+          name="name"
+          onChange={this.handleChange}
+        />
+      </form>
+    );
+  }
+}
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <NameForm />;
   }
 }
 
